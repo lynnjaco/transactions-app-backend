fafbfb3d8e8c13d33215ea5e987e287622061c08
lynnjaco <jacobs.lynnette@gmail.com>
2024-07-04 21:59:51 -0400

    app file set up with members and transactions controllers

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+const membersController = require("./controllers/membersController");
+app.use("/members", membersController);
+
+const transactionsController = require("./controllers/transactionsController");
+app.use("/transactions", transactionsController);
+
+module.exports = app;
