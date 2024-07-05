df674fcea04a128877fad84af0aee79e559db7ed
lynnjaco <jacobs.lynnette@gmail.com>
2024-07-04 21:52:38 -0400

    adds CRUD routes to members controller. members and transactions controllers not yet tested

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -2,9 +2,57 @@ const express = require("express");
 const members = express.Router();
 const membersArray = require("../models/memberModel.js");
 const { nanoid } = require("nanoid");
+const generateId = () => nanoid();
 
 members.get("/", (req, res) => {
   res.json(membersArray);
+});
+
+// Create a household member
+members.post("/", (req, res) => {
+  const newMember = {
+    id: generateId(),
+    ...req.body
+  };
+  membersArray.push(newMember);
+  res.status(201).send(newMember);
+});
+
+// Show a specific household member
+// GET A SINGLE WORKOUT
+members.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const member = membersArray.find(el => el.id === Number(id));
+  if (member) {
+    res.status(200).send(member)
+  } else {
+    res.status(404).json({error: `Member with ID: ${id} Not Found`})
+  }
+})
+
+// Update a household member
+members.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const memberIndex = membersArray.findIndex(member => member.id === Number(id));
+
+    if (memberIndex === -1) {
+        res.status(404).send({ error: `Member with ID: ${id} Not Found` });
+    } else {
+        membersArray[memberIndex] = { ...membersArray[memberIndex], ...req.body };
+        res.status(200).send(membersArray[memberIndex]);
+    }
+});
+
+// Delete a household member
+members.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const memberIndex = membersArray.findIndex(member => member.id === Numbers(id));
+    if (memberIndex === -1) {
+        res.status(404).send({ error: `Member with ID: ${id} Not Found` });
+    } else {
+        membersArray.splice(memberIndex, 1);
+        res.redirect("/members");
+    }
 });
 
 module.exports = members;
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -9,56 +9,60 @@ transactions.get("/", (req, res) => {
 });
 
 // Create a new transaction
-transactions.post('/', (req, res) => {
+transactions.post("/", (req, res) => {
   const newTransaction = {
     id: generateId(),
     ...req.body
   };
-  transactions.push(newTransaction);
+  transactionsArray.push(newTransaction);
   res.status(201).send(newTransaction);
 });
 
 // Update an existing transaction
 transactions.put("/:id", (req, res) => {
     const { id } = req.params;
-    const transactionIndex = transactions.findIndex(transaction => transaction.id === Number(id));
+    const transactionIndex = transactionsArray.findIndex(transaction => transaction.id === Number(id));
 
     if (transactionIndex === -1) {
         res.status(404).send({ error: `Transaction ${id} Not Found` });
     } else {
-        transactions[transactionIndex] = { ...transactions[transactionIndex], ...req.body };
-        res.status(200).send(transactions[transactionIndex]);
+        transactionsArray[transactionIndex] = { ...transactionsArray[transactionIndex], ...req.body };
+        res.status(200).send(transactionsArray[transactionIndex]);
     }
-  });
+});
 
   // Delete a transaction by ID
 transactions.delete("/:id", (req, res) => {
     const { id } = req.params;
-    const transactionIndex = transactions.findIndex(transaction => transaction.id === Numbers(id));
+    const transactionIndex = transactionsArray.findIndex(transaction => transaction.id === Numbers(id));
     if (transactionIndex === -1) {
         res.status(404).send({ error: `Transaction ${id} Not Found` });
     } else {
         transactionsArray.splice(transactionIndex, 1);
         res.redirect("/transactions");
     }
-  });
+});
 
 // Get all transactions
-  transactions.get("/", (req, res) => {
+transactions.get("/", (req, res) => {
     res.status(200).send(transactionsArray);
-  });
+});
 
 // Get all household/common transactions
 transactions.get("/household", (req, res) => {
-    const householdTransactions = transactions.filter(transaction => transaction.transactionMemberID === 'Household');
+    const householdTransactions = transactionsArray.filter(transaction => transaction.transactionMemberID === 'Household');
     res.status(200).send(householdTransactions);
-  });
+});
 
 // Get all transactions for a specific member
 transactions.get("/member/:memberId", (req, res) => {
     const { memberId } = req.params;
-    const memberTransactions = transactions.filter(transaction => transaction.transactionMemberId === Number(memberId));
-    res.status(200).send(memberTransactions);
-  });
+    if (transactionIndex === -1) {
+        res.status(404).send({ error: `Transaction ${id} Not Found` });
+    } else {
+        const memberTransactions = transactionsArray.filter(transaction => transaction.transactionMemberId === Number(memberId));
+        res.status(200).send(memberTransactions);
+    }
+});
 
 module.exports = transactions;
